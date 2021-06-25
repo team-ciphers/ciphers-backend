969aefcdbacff41387d1f579e12fea814d42b38c
Anas Al-Ramahi <anas_alramahi@outlook.com>
2021-06-25 11:35:14 +0300

    halfway throught the controller

--- controllers/Movie.controller.js
+++ controllers/Movie.controller.js
@@ -1,5 +1,14 @@
 "user strict";
 
+const { default: axios } = require('axios');
 const movieModel = require('../models/UserMovies.model')
 
-
+const getLatestMovies = async (request, response) => {
+    await axios.get('https://api.themoviedb.org/3/movie/latest?api_key=<<api_key>>&language=en-US').then.
+        if(error) {
+        response.send(error.message);
+    }
+        else {
+    response.json(user);
+}
+}
