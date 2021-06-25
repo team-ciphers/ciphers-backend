e4ba9031c4e230902cc1d7f6e55669acbb8165f8
majdkh97 <majd.motaz@yahoo.com>
2021-06-25 16:20:14 +0300

    Added movie get request + extra features

--- models/UserMovies.model.js
+++ models/UserMovies.model.js
@@ -1,15 +1,15 @@
 "use strict";
 
-const mongoose = require("mongoose");
-const MovieSchema = require("./Movies.model");
+// const mongoose = require("mongoose");
+// const MovieSchema = require("./Movies.model");
 
-const UserMovies = new mongoose.Schema({
-    email: { type: String },
-    favMovie: [MovieSchema],
-    to_watch: [MovieSchema]
-}) // this is dedicated for the profile page lists for the user.
+// const UserMovies = new mongoose.Schema({
+//     email: { type: String },
+//     favMovie: [MovieSchema],
+//     to_watch: [MovieSchema]
+// }) // this is dedicated for the profile page lists for the user.
 
-const movieModel = mongoose.model('moviesList', MovieSchema);
+// const movieModel = mongoose.model('moviesList', MovieSchema);
 
 
-module.exports = movieModel
+// module.exports = movieModel
--- server.js
+++ server.js
@@ -7,15 +7,18 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 
-
-
 app.use(cors());
 app.use(express.json());
 
+const {getTopRatedMovies,getPopularMovies,MovieSearchByName,getUpcomingMovies} = require('./controllers/Movie.controller');
 
 app.get('/',
     function (req, res) {
         res.send('Hello World');
     });
 
+app.get('/movies',MovieSearchByName);
+app.get('/moviesUpcoming',getUpcomingMovies)
+app.get('/moviesRated', getTopRatedMovies);
+app.get('/moviesPopular', getPopularMovies);
 app.listen(PORT);
