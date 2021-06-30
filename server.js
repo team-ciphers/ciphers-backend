1aae1e5f0ec8a47f33373a872bf709d140b21241
Anas Al-Ramahi <anas_alramahi@outlook.com>
2021-06-30 17:48:26 +0300

    mongodb deployment

--- server.js
+++ server.js
@@ -9,28 +9,28 @@ const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-const {createUser ,getUsers , createFavorite, createToWatch , deleteFav , deleteToWatch , createReview , getReviews , deleteReview}=require('./controllers/User.controller')
-const {getTopRatedMovies,getPopularMovies,MovieSearchByName,getUpcomingMovies,getTrailer} = require('./controllers/Movie.controller');
-const  {movieModel , seedUserModel}=require('./models/UserMovies.model');
-mongoose.connect('mongodb://localhost:27017/users', { useNewUrlParser: true, useUnifiedTopology: true });
+const { createUser, getUsers, createFavorite, createToWatch, deleteFav, deleteToWatch, createReview, getReviews, deleteReview } = require('./controllers/User.controller')
+const { getTopRatedMovies, getPopularMovies, MovieSearchByName, getUpcomingMovies, getTrailer } = require('./controllers/Movie.controller');
+const { movieModel, seedUserModel } = require('./models/UserMovies.model');
+mongoose.connect(`mongodb://Cipher:${process.env.MONGO_DB}@cluster0-shard-00-00.g6kih.mongodb.net:27017,cluster0-shard-00-01.g6kih.mongodb.net:27017,cluster0-shard-00-02.g6kih.mongodb.net:27017/MyMovieDatabase?ssl=true&replicaSet=atlas-6l6686-shard-0&authSource=admin&retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 app.get('/',
     function (req, res) {
         res.send('Hello World');
     });
 
-app.get('/movies',MovieSearchByName);
-app.get('/moviesUpcoming',getUpcomingMovies)
+app.get('/movies', MovieSearchByName);
+app.get('/moviesUpcoming', getUpcomingMovies)
 app.get('/moviesRated', getTopRatedMovies);
 app.get('/moviesPopular', getPopularMovies);
 
 app.post('/users', createUser)
-app.get('/users',getUsers);
+app.get('/users', getUsers);
 
-app.post('/usersFav',createFavorite);
-app.delete('/usersFav/:idx',deleteFav);
+app.post('/usersFav', createFavorite);
+app.delete('/usersFav/:idx', deleteFav);
 
-app.post('/usersWatch',createToWatch);
-app.delete('/usersWatch/:idx',deleteToWatch);
+app.post('/usersWatch', createToWatch);
+app.delete('/usersWatch/:idx', deleteToWatch);
 
 app.get('/userReview', getReviews);
 app.post('/userReview', createReview);
